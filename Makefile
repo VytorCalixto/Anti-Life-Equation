4689d7ab43ea0f449956d0702ebe0b3caa94018b
Vytor Calixto <vytorcalixto@gmail.com>
2015-12-17 18:03:23 -0200

    Updated Makefile and test script

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 CFLAGS=-I/home/soft/likwid/include -llikwid -L/usr/lib -L/home/soft/likwid/lib -lm -std=c99 -Wall -DLIKWID_PERFMON -O3 -march=native -mavx -fstrict-aliasing
 
-all: main.c
-	gcc -o pdeSolver main.c $(CFLAGS)
+all: main-new.c main-old.c
+	gcc -o pdeSolver main-new.c $(CFLAGS)
+	gcc -o oldSolver main-old.c $(CFLAGS)
 
 clean:
-	rm -fr *.o pdeSolver
+	rm -fr *.o *Solver
--- test.sh
+++ test.sh
@@ -1,39 +1,43 @@
 make
 
-HEADER="#Points SOR Residual"
+HEADER="#Points\tNewSOR\tNewResidual\tOldSOR\tOldResidual"
 
-echo -e "$HEADER\n" > timePlot
-echo -e "$HEADER\n" > memPlot
-echo -e "$HEADER\n" > cachePlot
-echo -e "$HEADER\n" > flopsPlot
+#echo -e "$HEADER\n" > timePlot
+#echo -e "$HEADER\n" > memPlot
+#echo -e "$HEADER\n" > cachePlot
+#echo -e "$HEADER\n" > flopsPlot
 
-for i in 10 100 127 128 200 255 256 500 511 512 1000 1023 1024 2000 2047 2048
+for i in 2000 2047 2048
 do
     echo "$i points"
     PI=$(echo "scale=20; 355/113" | bc)
-    INTERVAL=$(echo "scale=20; $PI/($i-1)" | bc)
+    INTERVAL=$(echo "scale=8; $PI/($i-1)" | bc)
     echo "TIME"
+    ./oldSolver -hx $INTERVAL -hy $INTERVAL -i 20 -o timeOld
     ./pdeSolver -hx $INTERVAL -hy $INTERVAL -i 20 -o time
-    TIME=$(cat time | grep Tempo | cut -d ':' -f 2 | tr -d ' ' | tr '\n' '\t')
+    TIME=$(cat time timeOld | grep Tempo | cut -d ':' -f 2 | tr -d ' ' | tr '\n' '\t')
     echo -e "$i\t$TIME" >> timePlot
     
     echo "MEM"
     likwid-perfctr -m -f -C 2 -g MEM ./pdeSolver -hx $INTERVAL -hy $INTERVAL -i 20 -o mem > MEM
-    TEXT=$(cat MEM | grep bandwidth | cut -d '|' -f 3 | tr -d ' ' | tr '\n' '\t')
+    likwid-perfctr -m -f -C 5 -g MEM ./oldSolver -hx $INTERVAL -hy $INTERVAL -i 20 -o mem > MEMOLD
+    TEXT=$(cat MEM MEMOLD | grep bandwidth | cut -d '|' -f 3 | tr -d ' ' | tr '\n' '\t')
     echo -e "$i\t$TEXT" >> memPlot
-    rm MEM
+    rm MEM MEMOLD
     
     echo "CACHE"
     likwid-perfctr -m -f -C 3 -g CACHE ./pdeSolver -hx $INTERVAL -hy $INTERVAL -i 20 -o cache > CACHE
-    TEXT=$(cat CACHE | grep "data cache miss ratio" | cut -d '|' -f 3 | tr -d ' ' | tr '\n' '\t')
+    likwid-perfctr -m -f -C 0 -g CACHE ./oldSolver -hx $INTERVAL -hy $INTERVAL -i 20 -o cache > CACHEOLD
+    TEXT=$(cat CACHE CACHEOLD | grep "data cache miss ratio" | cut -d '|' -f 3 | tr -d ' ' | tr '\n' '\t')
     echo -e "$i\t$TEXT" >> cachePlot
-    rm CACHE
+    rm CACHE CACHEOLD
 
     echo "FLOPS"
     likwid-perfctr -m -f -C 5 -g FLOPS_DP ./pdeSolver -hx $INTERVAL -hy $INTERVAL -i 20 -o flops >> FLOPS
-    TEXT=$(cat FLOPS | grep "DP MFLOP/s" | cut -d '|' -f 3 | tr -d ' ' | tr '\n' '\t')
+    likwid-perfctr -m -f -C 1 -g FLOPS_DP ./oldSolver -hx $INTERVAL -hy $INTERVAL -i 20 -o flops >> FLOPSOLD
+    TEXT=$(cat FLOPS FLOPSOLD | grep "DP MFLOP/s" | cut -d '|' -f 3 | tr -d ' ' | tr '\n' '\t')
     echo -e "$i\t$TEXT" >> flopsPlot
-    rm FLOPS
+    rm FLOPS FLOPSOLD
 done
 
-rm time mem cache flops
+rm time timeOld mem cache flops
